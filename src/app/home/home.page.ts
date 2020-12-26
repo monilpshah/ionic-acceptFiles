49af35ae49251f330d0b713d81d28c46bc8f2d67
monilpshah <monil31199@gmail.com>
2020-12-26 17:01:37 +0530

    accept files from camera and filestorage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NavController, ActionSheetController, LoadingController } from '@ionic/angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera/ngx';
+import * as Tesseract from 'tesseract.js'
+import { NgProgress } from '@ngx-progressbar/core';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  selectedImage: string;
+  imageText: string;
+ 
+  constructor(public navCtrl: NavController, private camera: Camera, private actionSheetCtrl: ActionSheetController, public progress: NgProgress) {
+  }
+ 
+  async selectSource() {    
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+ 
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = 'data:image/jpeg;base64,${imageData}';
+    });
+  }
+ 
+  recognizeImage() {
+  //   Tesseract.recognize(this.selectedImage)
+  //   .catch(err => console.error(err))
+  //   .then(result => {
+  //     this.imageText = result.text;
+  //   })
+  //   .finally(result => {
+  //     this.progress.complete();
+  //   });
+  }
 }
